d21e2d209194493beb46a1b2790115cd1dd8b978
Redwanraw <82659185+Redwanraw@users.noreply.github.com>
2022-03-23 16:54:56 +0200

    Update Login.js

--- src/Componet/Login.js
+++ src/Componet/Login.js
@@ -2,10 +2,12 @@ import React , {useState} from "react"
 import registration from "../img/registration.png"
 import lock from "../img/lock.png" 
 import add_user from "../img/add-user.png" 
+import Register from "./Register"
 export default function Login(props){
-
+const [isLoginPage,setisLoginPage]=useState(true)
     return(
         <div className="Login">
+
         <form className=" mt-5 w-50 text-center position-absolute top-50 start-50 translate-middle " id="forms" action="/index.html">
         <img className="w-50 my-2" src={registration} />
             <div className="input-group mb-3">
@@ -18,9 +20,14 @@ export default function Login(props){
             </div>
             <div className="text-start">
                 <img src={lock} /> <a href="" className="text-dark me-2 font ">Forgot your password </a>
-                <img src={add_user} /> <a href="" className="text-dark  font">Create a new account </a>
+
+                        <img src={add_user} /> <a onClick={()=>{props.onClick()}} className="text-dark  font">Create a new account </a>
+
             </div>
-            <button type="button d-block" className="btn btn-light">Light</button>
+            <button type="button d-block" className="btn btn-light" onClick={(e)=>{
+                setisLoginPage(false)
+                e.preventDefault()
+                }}>Light</button>
 
             </form>
         </div>

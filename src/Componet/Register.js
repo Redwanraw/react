2347350b54b0fd8d691c2b06841945bd596e0c7c
Redwanraw <82659185+Redwanraw@users.noreply.github.com>
2022-03-23 16:54:08 +0200

    Update Register.js

--- src/Componet/Register.js
+++ src/Componet/Register.js
@@ -2,12 +2,12 @@ import React , {useState} from "react"
 import registration from "../img/registration.png"
 import lock from "../img/lock.png" 
 import add_user from "../img/add-user.png" 
-
+import Login from "./Login"
 export default function Register(props){
-
+const [isLog,setisLog]=useState(true)
     return(
         <div className="Register">
-        <form className=" mt-5 w-50 text-center position-absolute top-50 start-50 translate-middle " id="forms" action="/index.html">
+        <form className=" mt-5 w-50 text-center position-absolute top-50 start-50 translate-middle " id="forms" >
         <img className="w-50 my-2" src={registration} />
             <div className="input-group mb-3">
                 <span className="input-group-text " id="basic-addon1">@</span>
@@ -19,12 +19,16 @@ export default function Register(props){
             </div>
             <div className="input-group mb-3">
                 <span className="input-group-text " id="basic-addon1">🔑</span>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                <input type="text" className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon1" />
             </div>
 
             <div className="text-start">
-                <img src={lock} /> <a href="" className="text-dark me-2 font ">Forgot your password </a>
-                <img src={add_user} /> <a href="" className="text-dark  font">Create a new account </a>
+                <img src={lock} /> <a  href="" className="text-dark me-2 font ">Forgot your password </a>
+           
+                        <img src={add_user} /> <a href="" onClick={(e)=>{
+                            props.onClick(e)
+                            e.preventDefault()}}  className="text-dark  font">login </a>
+
             </div>
             <button type="button d-block" className="btn btn-light">Light</button>
 
